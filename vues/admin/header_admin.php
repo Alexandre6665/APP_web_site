e66d6148ee28cc1388e1a109387391f8452fb123
AlexT6665 <taingalexandre75@outlook.fr>
2024-05-28 11:32:38 +0200

    Commit footer_admin Commit header_admin Commit accueil_admin

---
+++ vues/admin/header_admin.php
@@ -0,0 +1,35 @@
+<header class="header">
+        <a class="logo" href=""><img src="../../images/Logo_SoundOnAir.png" alt="Logo" width="130px"
+                height="20px"></a>
+
+        <nav class="navbar">
+            <button><a href=""><span>UTILISATEURS</span></a></button>
+            <button><a href=""><span>FILMS</span></a></button>
+            <button><a href=""><span>FAQ</span></a></button>
+            <button><a href=""><span>CGU</span></a></button>
+            <button><a href=""><span>MENTIONS LÉGALES</span></a></button>
+            <div class="dropdown">
+                <button><a href=""><span>COMPTE</span></a>
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="">Gérer mon compte</a>
+                    <a href="">Déconnexion</a>
+                </div>
+            </div>
+
+            <div class="lang-menu">
+                <div class="selected-lang">
+                    Français
+                </div>
+                <ul>
+                    <li>
+                        <a href="#" class="fr">Français</a>
+                    </li>
+                    <li>
+                        <a href="#" class="us">English</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
