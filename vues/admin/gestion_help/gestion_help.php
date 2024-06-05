afc6aecc8cdfd13ff90f228c920e2a0eb0f3ef90
AlexT6665 <taingalexandre75@outlook.fr>
2024-06-05 23:02:35 +0200

    Commit gestion_help

--- vues/admin/gestion_help/gestion_help.php
+++ vues/admin/gestion_help/gestion_help.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang fr>
 <head>
-    <title>Administrateur</title>
+    <title>Message</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="accueil_admin.css">
+    <link rel="stylesheet" href="gestion_help.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
@@ -13,7 +13,7 @@
 </head>
 
 <body>
-    
+
     <?php
     include '../header_admin.php';
     ?>
@@ -21,6 +21,33 @@
 <main>
     <h1>Liste des utilisateurs</h1>
     <hr>
+    <?php
+    include 'connectToDB_help.php';
+    $sql = 'SELECT id_message, nom, prenom, mail, objet, content FROM message';
+    $result = $conn->query($sql);
     
+    if ($result->num_rows > 0) {
+        echo '<table>';
+        echo '<tr><th>ID</th><th>Nom</th><th>Prénom</th><th>Email</th><th>Objet</th><th>Message</th></tr>';
+        
+        // Afficher les résultats
+        while ($row = $result->fetch_assoc()) {
+            echo '<tr>';
+            echo '<td>' . htmlspecialchars($row['id_message']) . '</td>';
+            echo '<td>' . htmlspecialchars($row['nom']) . '</td>';
+            echo '<td>' . htmlspecialchars($row['prenom']) . '</td>';
+            echo '<td>' . htmlspecialchars($row['mail']) . '</td>';
+            echo '<td>' . htmlspecialchars($row['objet']) . '</td>';
+            echo '<td>' . htmlspecialchars($row['content']) . '</td>';
+            echo '</tr>';
+        }
+        echo '</table>';
+    } else {
+        echo 'Aucun message trouvé.';
+    }
+    ?>
 </main>
+<?php 
+include '../footer_admin.php';
+?>
 </body>
--- vues/admin/header_admin.php
+++ vues/admin/header_admin.php
@@ -5,6 +5,7 @@
         <nav class="navbar">
             <button><a href="../accueil_admin/accueil_admin.php"><span>UTILISATEURS</span></a></button>
             <button><a href="../gestion_films/gestion_films.php"><span>FILMS</span></a></button>
+            <button><a href="../gestion_help/gestion_help.php"><span>Message</span></a></button>
             <button><a href="../FAQ_gestion/faq_admin.php"><span>FAQ</span></a></button>
             <button><a href="../CGU_gestion/cgu_admin.php"><span>CGU</span></a></button>
             <button><a href="../gestion_ML/gestion_mentions_legales.php"><span>MENTIONS LÉGALES</span></a></button>
