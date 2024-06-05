8375ee66516c5bae413f435709c2472ab59bc56d
AlexT6665 <taingalexandre75@outlook.fr>
2024-06-05 22:27:53 +0200

    Commit footer

--- vues/footer.php
+++ vues/footer.php
@@ -3,12 +3,12 @@
             <a href="#">Besoin d'aide ?</a>
             <a href="../help/help.php">Contact</a>
             <a href="">En savoir plus sur nous ?</a>
-
+            <a href="">Recherche</a>
         </div>
         <div class="footer-2">
             <a id="conditions" href="../cgu/cgu.php">Conditions Générales</a>
             <a id="faq" href="../FAQ/faq_spectateur.php">FAQ</a>
             <a id="mentions" href="../mentions_legales/mentions_legales.php">Mentions légales</a>
-
+            <a href="">Cookies</a>
         </div>
     </footer>
